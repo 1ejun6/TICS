9da87b9322bd43590b4cc8c6b91b90336603ea22
Jun <lowyinjin@gmail.com>
2023-02-18 15:02:03 +0800

    set-up my menu with layout and css

--- app/src/main/java/com/example/tics/ui/students/StudentsFragment.java
+++ app/src/main/java/com/example/tics/ui/students/StudentsFragment.java
@@ -1,4 +1,4 @@
-package com.example.tics.ui.home;
+package com.example.tics.ui.students;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,22 +10,22 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.tics.databinding.FragmentHomeBinding;
+import com.example.tics.databinding.FragmentStudentsBinding;
 
-public class HomeFragment extends Fragment {
+public class StudentsFragment extends Fragment {
 
-    private FragmentHomeBinding binding;
+    private FragmentStudentsBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+        com.example.tics.ui.students.StudentsViewModel gameViewModel =
+                new ViewModelProvider(this).get(com.example.tics.ui.students.StudentsViewModel.class);
 
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        binding = FragmentStudentsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        final TextView textView = binding.textStudents;
+        gameViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
